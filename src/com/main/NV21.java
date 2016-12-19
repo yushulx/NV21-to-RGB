09a6410c55f2bb05940c447a872491e3b7aff7b3
yushulx <lingxiao1002@gmail.com>
2016-12-19 13:40:26 +0800

    Reverse U and V order

--- src/com/main/NV21.java
+++ src/com/main/NV21.java
@@ -102,8 +102,8 @@ public class NV21 {
 				if (Y < 0) Y += 255;
 				
 				if ((x & 1) == 0) {
-					Cb = yuv[(y >> 1) * (width) + x + total];
-					Cr = yuv[(y >> 1) * (width) + x + total + 1];
+					Cr = yuv[(y >> 1) * (width) + x + total];
+					Cb = yuv[(y >> 1) * (width) + x + total + 1];
 					
 					if (Cb < 0) Cb += 127; else Cb -= 128;
 					if (Cr < 0) Cr += 127; else Cr -= 128;
@@ -113,11 +113,16 @@ public class NV21 {
 				G = Y - (Cb >> 2) + (Cb >> 4) + (Cb >> 5) - (Cr >> 1) + (Cr >> 3) + (Cr >> 4) + (Cr >> 5);
 				B = Y + Cb + (Cb >> 1) + (Cb >> 2) + (Cb >> 6);
 				
+				// Approximation
+//				R = (int) (Y + 1.40200 * Cr);
+//			    G = (int) (Y - 0.34414 * Cb - 0.71414 * Cr);
+//				B = (int) (Y + 1.77200 * Cb);
+				
 				if (R < 0) R = 0; else if (R > 255) R = 255;
 				if (G < 0) G = 0; else if (G > 255) G = 255;
 				if (B < 0) B = 0; else if (B > 255) B = 255;
 				
-				rgb[index++] = 0xff000000 + (B << 16) + (G << 8) + R;
+				rgb[index++] = 0xff000000 + (R << 16) + (G << 8) + B;
 			}
 		}
 		
